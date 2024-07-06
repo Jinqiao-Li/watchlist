1ff455b6f6afb833a693a0093a6b1d3e266c2b8f
Leee <jinqiaoleee@gmail.com>
2024-07-06 15:45:53 +0200

    Add base template and error template

--- app.py
+++ app.py
@@ -48,13 +48,21 @@ def initdb(drop):
     db.create_all()
     click.echo('Initialized database.')
 
+# use in all templates
+@app.context_processor
+def inject_user():
+    user = User.query.first()
+    return dict(user=user)
 
+@app.errorhandler(404)  # error code
+def page_not_found(e):  # 接受异常对象作为参数
+    return render_template('404.html'), 404  # return template and status code
 
 @app.route('/')
 def index():
-    user = User.query.first()
     movies = Movie.query.all()
-    return render_template('index.html', user=user, movies=movies)
+    return render_template('index.html', movies=movies)
+
 
 class User(db.Model):  # table name: user（自动生成，小写处理）
     id = db.Column(db.Integer, primary_key=True)  # 主键
@@ -66,17 +74,4 @@ class Movie(db.Model):  # table name: movie
     title = db.Column(db.String(60))  # 电影标题
     year = db.Column(db.String(4))  # 电影年份
 
-name = 'Leee'
-movies = [
-    {'title': 'My Neighbor Totoro', 'year': '1988'},
-    {'title': 'Dead Poets Society', 'year': '1989'},
-    {'title': 'A Perfect World', 'year': '1993'},
-    {'title': 'Leon', 'year': '1994'},
-    {'title': 'Mahjong', 'year': '1996'},
-    {'title': 'Swallowtail Butterfly', 'year': '1996'},
-    {'title': 'King of Comedy', 'year': '1999'},
-    {'title': 'Devils on the Doorstep', 'year': '1999'},
-    {'title': 'WALL-E', 'year': '2008'},
-    {'title': 'The Pork of Music', 'year': '2012'},
-]
 
